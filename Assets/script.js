6604701a9c118ea5621355824d2da01411482c8b
Jacob Lewis <jacobllewis@outlook.com>
2022-01-10 22:47:38 -0700

    simplified variable names, finalized comments. No further edits at this time. - JL

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,10 @@
 // Assigning variables
 var generateBtn = document.querySelector("#generate");
-var lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lettersLower = "abcdefghigjklmopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghigjklmopqrstuvwxyz";
 var numbers = "0123456789";
 var symbols = " !#$%()*+,-./:;<=>?'@[&]^_`{|}~";
-var characters = "";
+var allowed = "";
 
 // Write password function to recieve inputs, operate generatePassword function & display output
 function writePassword() {
@@ -22,32 +22,32 @@ function writePassword() {
   if (pwLength <= 7 || pwLength >=129) return alert("Must be a number between 8 and 128");
 
   // Recieve further inputs/criteria for password
-  var includeUpper = confirm("Do you want uppercase letters in your password?");
-  var includeLower = confirm("Do you want lowercase letters in your password?");
-  var includeNumbers = confirm("Do you want numbers in your password?");
-  var includeSymbols = confirm("Do you want symbols in your password?");
+  var incLower = confirm("Do you want lowercase letters in your password?");
+  var incUpper = confirm("Do you want uppercase letters in your password?");
+  var incNumbers = confirm("Do you want numbers in your password?");
+  var incSymbols = confirm("Do you want symbols in your password?");
 
   // Function to generate the password
-  function generatePassword(length, chars) {
+  function generatePassword(length, characters) {
 
     // Creating base password to include one of each peramiter set by user(base) & Creating list of allowed characters based on paramiters set by user (char)
-    includeLower === true ? ((base += lettersLower.charAt(Math.floor(Math.random() * lettersLower.length))), (chars += lettersLower)) : "";
-    includeUpper === true ? (base += lettersUpper.charAt(Math.floor(Math.random() * lettersUpper.length)), (chars += lettersUpper)) : "";
-    includeNumbers === true ? (base += numbers.charAt(Math.floor(Math.random() * numbers.length)), (chars += numbers)) : "";
-    includeSymbols === true ? (base += symbols.charAt(Math.floor(Math.random() * symbols.length)), (chars += symbols)) : "";
+    incLower === true ? ((base += lower.charAt(Math.floor(Math.random() * lower.length))), (characters += lower)) : "";
+    incUpper === true ? (base += upper.charAt(Math.floor(Math.random() * upper.length)), (characters += upper)) : "";
+    incNumbers === true ? (base += numbers.charAt(Math.floor(Math.random() * numbers.length)), (characters += numbers)) : "";
+    incSymbols === true ? (base += symbols.charAt(Math.floor(Math.random() * symbols.length)), (characters += symbols)) : "";
 
     // Creating for loop to randomize password. If i is less than the requested length (less base length to ensure sufficient length) continue the loop, increasing by one each time
     // Picks number between 0 and length of allowed characters and adds it to password until it is of sufficient length including base
     for (let i = base.length; i < length; i++) {
-      password += chars.charAt(
-        Math.floor(Math.random() * chars.length)
+      password += characters.charAt(
+        Math.floor(Math.random() * characters.length)
       );
     }
     return password;
   }
 
   // After prompts/confirmations & rejections, run generatePassword function to generate the password based on inputs
-  generatePassword(pwLength, characters);
+  generatePassword(pwLength, allowed);
 
   // Outputs to page, console and alert
   passwordText.value = (password + base);
@@ -55,5 +55,5 @@ function writePassword() {
   alert(password + base);
 }
 
-// Add event listener to generate button
+// Add event listener to generate button - activates writePassword function on click of button
 generateBtn.addEventListener("click", writePassword);
